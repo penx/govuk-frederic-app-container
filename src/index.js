0f6319bff810b8d4d771f5020b73d914145572d6
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-05 16:03:08 +0100

    displayName prop and nav

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 // TODO: feature flags
 const features = {
@@ -32,6 +32,7 @@ export default class ExampleComponent extends Component {
   static propTypes = {
     modules: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string,
+      displayName: PropTypes.string,
       render: PropTypes.function
     }))
   }
@@ -44,7 +45,9 @@ export default class ExampleComponent extends Component {
     return (
       <div>
         <div>Top Nav</div>
-        <div>Active Module</div>
+        <div>
+          {modules.map(({name, displayName}) => <Link to={`/${name}`}>{displayName}</Link>)}
+        </div>
         <Router>
           <Switch>
             {modules.map(({name, render}) => <Route key={name} path={`/${name}`}

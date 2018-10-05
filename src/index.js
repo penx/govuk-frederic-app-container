58fc752ed8d2ac39d1700356420e4e3437a20655
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-05 14:12:56 +0100

    Initial router and reference data plan

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import styles from './styles.css'
+// TODO: feature flags
+const features = {
+  feature1: true,
+  feature2: false
+}
+
+// TODO: auth
+const auth = {
+  user: {
+    name: 'Jeff Goldblum'
+  }
+}
+
+// TODO: reference data
+
+const reference = {
+  countries: {
+    'GB': 'UK',
+    'US': 'USA'
+  }
+}
+
+function getReference(key) {
+  // TODO:
+}
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    modules: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      render: PropTypes.function
+    }))
   }
 
   render() {
     const {
-      text
+      modules
     } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>
+        <div>Top Nav</div>
+        <div>Active Module</div>
+        <Router>
+          {modules.each(({name, render}) => <Route path={`/${name}`}
+            render={({match, location, history}) => render({match, location, history, features, auth, reference, getReference})}
+          />)}
+        </Router>
       </div>
     )
   }
